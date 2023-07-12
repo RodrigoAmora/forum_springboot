c8c3c982f100f155fc8e05cd64c2f5414ca6a1b3
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-12 17:34:12 -0300

    Mapeando as classes no banco

--- src/main/java/br/com/rodrigoamora/forum/model/Resposta.java
+++ src/main/java/br/com/rodrigoamora/forum/model/Resposta.java
@@ -2,39 +2,32 @@ package br.com.rodrigoamora.forum.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "resposta")
 public class Resposta {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "topico_id")
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "autor_id")
 	private Usuario autor;
 	private Boolean solucao = false;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Resposta other = (Resposta) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 
 	public Long getId() {
 		return id;
